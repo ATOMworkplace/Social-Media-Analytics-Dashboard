10ec9b77fb4607f2c4986998d5631e5ce8efd41b
/Charchit_Gangwar/ <114564628+ATOMworkplace@users.noreply.github.com>
2024-06-17 12:48:22 +0530

    Delete backend/django-react-auth-main directory

--- backend/django-react-auth-main/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView
-from users.views import SignUpView
-
-urlpatterns = [
-    path('signup/', SignUpView.as_view(template_name='signup.html', success_url='/signin/'), name='signup'),
-    path('signin/', LoginView.as_view(template_name='signin.html'), name='signin'),
-]
